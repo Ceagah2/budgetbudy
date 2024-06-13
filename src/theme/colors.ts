dfdf438337678bcefa363804e4ec60bd722af100
Carlos Ceagah <carlosceagah@gmail.com>
2024-06-13 12:31:58 -0300

    style: create colors theme file

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  royalBlue: "#2A4365",
+  lightBlue: "#63B3ED",
+  accentOrange: "#F6AD55",
+  background: "#F7FAFC",
+  text: "#2D3748",
+  errorRed: "#E53E3E",
+  successGreen: "#38A169",
+};
