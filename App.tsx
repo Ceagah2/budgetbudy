15365001b2c7d6595bc95025e46d7a7a3adb9e1b
Carlos Ceagah <carlosceagah@gmail.com>
2024-06-13 12:41:23 -0300

    feat: configure useFonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
+import { Lato_300Light, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+import { PlayfairDisplay_400Regular, PlayfairDisplay_500Medium } from '@expo-google-fonts/playfair-display';
+import { RobotoSlab_300Light, RobotoSlab_400Regular, RobotoSlab_500Medium } from '@expo-google-fonts/roboto-slab';
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_700Bold,
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_500Medium,
+    RobotoSlab_300Light,
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium
+  })
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
